72543b36d4db46e69f05b19d57436be6abdb9665
CaolanCode <cmacmahon3@gmail.com>
2022-12-03 19:36:14 +0000

    initial commit of rucksack file

---
+++ rucksack.py
@@ -0,0 +1,7 @@
+# Rearraging rucksacks
+# Every item is identified by a lowercase or uppercase character
+# A list of items in a rucksack is given as a line of characters
+# The first half of the characters is the first compartment and the second half is the second compartment
+# Find the item that appears in both compartments of the bag
+# Priority for a - z: 1 - 26
+# Priority for A - Z: 27 - 52
