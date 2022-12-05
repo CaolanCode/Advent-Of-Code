de18b679c2c748e4573020e082b74b97fddd8c6e
CaolanCode <cmacmahon3@gmail.com>
2022-12-05 21:40:48 +0000

    commit day five part one complete

--- day_five_I.py
+++ day_five_I.py
@@ -2,12 +2,35 @@
 # given number of elements to move from one stack to another
 # When finished report the elements on top of the array
 # Combine the elements together
-list_one = ['V','C','D','R','Z','G','B','W']
-list_two = ['G','W','F','C','B','S','T','V']
-list_three = ['C','B','S','N','W']
-list_four = ['Q','G','M','N','J','V','C','P']
-list_five = ['T','S','L','F','D','H','B']
-list_six = ['J','V','T','W','M','N']
-list_seven = ['P','F','L','C','S','T','G']
-list_eight = ['B','D','Z']
-list_nine = ['M','N','Z','W']
+Stack = [
+     ['V','C','D','R','Z','G','B','W'],
+ ['G','W','F','C','B','S','T','V'],
+ ['C','B','S','N','W'],
+ ['Q','G','M','N','J','V','C','P'],
+ ['T','S','L','F','D','H','B'],
+ ['J','V','T','W','M','N'],
+ ['P','F','L','C','S','T','G'],
+ ['B','D','Z'],
+ ['M','N','Z','W']
+]
+
+
+file = open("day_five.txt", 'r').read().strip()
+lines = [x.strip() for x in file.split('\n')]
+
+for command in lines:
+    words = command.split()
+    qty = int(words[1])
+    from_stack = int(words[3])
+    to_stack = int(words[5])
+    i = 0
+    while i < qty:
+        move = Stack[from_stack-1].pop()
+        Stack[to_stack-1].append(move)
+        i += 1
+
+ends = ""
+for lines in Stack:
+    ends = ends + lines[len(lines)-1]
+
+print(ends)
