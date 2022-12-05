2d79a8c8ad9909322cd9cf9e18e608779ff26156
CaolanCode <cmacmahon3@gmail.com>
2022-12-05 11:03:09 +0000

    initial commit of day five file

---
+++ day_five_I.py
@@ -0,0 +1,13 @@
+# Rearrange stacks
+# given number of elements to move from one stack to another
+# When finished report the elements on top of the array
+# Combine the elements together
+list_one = ['V','C','D','R','Z','G','B','W']
+list_two = ['G','W','F','C','B','S','T','V']
+list_three = ['C','B','S','N','W']
+list_four = ['Q','G','M','N','J','V','C','P']
+list_five = ['T','S','L','F','D','H','B']
+list_six = ['J','V','T','W','M','N']
+list_seven = ['P','F','L','C','S','T','G']
+list_eight = ['B','D','Z']
+list_nine = ['M','N','Z','W']
